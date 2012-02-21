eef9a0db67c82ea418d302f3fda583c0d5e1a00c
Your Name <nilesh@ashra.co.uk>
2012-02-21 11:44:03 -0800

    Adding the example script to the repo.

---
+++ stream.py
@@ -0,0 +1,19 @@
+from twitter_easy_streamer.streamer import Rule, RuleListener
+
+def print_message(tweet):
+    print tweet.text
+
+def start():
+    listener = RuleListener()
+
+    AUTH = {
+        'consumer_key': '9ejfkiCu8dTMNDzF29cg',
+        'consumer_secret': 'SicnRgAMXu31xvJnpJtqtGOZEZazhDynb7sXLPTnNo',
+        'access_token': '14112449-ZDgvgw5Hgj1hf1sZFWP0VSBnJBMOiVhbhgpNDWd5n',
+        'access_token_secret': 'ZMtTEXGZ5aNnGrMF03l0lSkqCaouo0CjJbEiN41QlO4'
+    }
+
+    rules = [Rule(track=["football"], on_status=[print_message])]
+    listener.listen(rules=rules, **AUTH)
+
+start()
