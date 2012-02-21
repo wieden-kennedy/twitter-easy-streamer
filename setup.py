90fc91e5cbb6d52cbe4965943db74fdfe093a39f
Your Name <nilesh@ashra.co.uk>
2012-02-20 18:25:08 -0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='twitter-easy-streamer',
+        version='0.0.1',
+        description='Easily stream tweets using tweepy',
+        long_description=open('README.md').read(),
+        author='Wieden+Kennedy',
+        author_email='nilesh.ashra@wk.com',
+        url='https://github.com/wieden-kennedy/twitter-easy-streamer/',
+        packages=find_packages(),
+        include_package_data=True,
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    )
